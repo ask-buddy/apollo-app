9d419168b3796a26b531830d9a370af39be9c405
Gregorius Yuristama <dev@gregoriusyuristama.me>
2024-08-11 12:38:40 +0700

    style: add colors (#12)

--- lib/Core/Constants/colors.dart
+++ lib/Core/Constants/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ABColors {
+extension ABColors on Colors {
 // Basic Color
   static const Color primary = Color(0xFF007BFF);
   static const Color secondary = Color(0xFFFF9900);
@@ -8,7 +8,26 @@ class ABColors {
 
   // Basic Dark
   static const Color primaryDark = Color(0xFF0069D9);
+  static const Color secondaryDark = Color(0xFFCC8800);
+  static const Color accentDark = Color(0xFF70A800);
+
+  // Basic Light
+  static const Color primaryLight = Color(0XFF82B1FF);
+  static const Color secondaryLight = Color(0xFFFFDC50);
+  static const Color accentLight = Color(0xFFD2FF8C);
+
+  // Variant
+  static const Color primaryBlue = Color(0xFF40A9FF);
+  static const Color secondaryGoldYellow = Color(0xFFFFC107);
+  static const Color deepSea = Color(0xFF0D1926);
+  static const Color errorBrightRed = Color(0xFFFF3333);
 
   // Neutral
+  static const Color black = Color(0xFF0D0D0D);
+  static const Color darkGray = Color(0xFF333333);
+  static const Color clay = Color(0xFF666666);
+  static const Color concrete = Color(0xFF999999);
   static const Color stone = Color(0xFFBDBDBD);
+  static const Color smoke = Color(0xFFF2F2F2);
+  static const Color white = Color(0xFFFFFFFF);
 }
